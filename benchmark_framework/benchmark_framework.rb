6b3c05f31f065a9bb1435a814bb61f454e158465
Jon Kimble <jonathanbkimble@gmail.com>
2017-09-08 16:14:53 -0700

    Graphing functionality finished

--- benchmark_framework/benchmark_framework.rb
+++ benchmark_framework/benchmark_framework.rb
@@ -1,5 +1,7 @@
 require 'benchmark'
+require 'code_to_benchmark'
 require 'pry'
+require 'gruff'
 
 TEN = 10
 HUNDRED = 100
@@ -9,31 +11,37 @@ HUNDRED_THOUSAND = 100_000
 MILLION = 1_000_000
 TEN_MILLION = 10_000_000
 
-Benchmark.bm(7) do |x|
-  x.report('report_label_1') { MILLION.times { some_code_to_execute } }
-  x.report('report_label_2') { MILLION.times { some_code_to_execute } }
-  x.report('report_label_3') { MILLION.times { some_code_to_execute } }
-  x.report('report_label_4') { MILLION.times { some_code_to_execute } }
-end
+# For comparing code that performs the same function
+
+# Benchmark.bm(7) do |x|
+#   x.report('report_label_1') { MILLION.times { some_code_to_execute } }
+#   x.report('report_label_2') { MILLION.times { some_code_to_execute } }
+#   x.report('report_label_3') { MILLION.times { some_code_to_execute } }
+#   x.report('report_label_4') { MILLION.times { some_code_to_execute } }
+# end
 
 # For testing O(n)
 
-# Benchmark.bm(7) do |x|
-#   x.report('10') { TEN.times { 1 + 1 } }
-#   x.report('100') { HUNDRED.times { 1 + 1 } }
-#   x.report('1,000') { THOUSAND.times { 1 + 1 } }
-#   x.report('10,000') { TEN_THOUSAND.times { 1 + 1 } }
-#   x.report('100,000') { HUNDRED_THOUSAND.times { 1 + 1 } }
-#   x.report('1,000,000') { MILLION.times { 1 + 1 } }
-#   x.report('10,000,000') { TEN_MILLION.times { 1 + 1 } }
-#   binding.pry
-# end
+graph_points = nil
+Benchmark.bm(7) do |x|
+  x.report('10') { TEN.times { 1 + 1 } }
+  x.report('100') { HUNDRED.times { 1 + 1 } }
+  x.report('1,000') { THOUSAND.times { 1 + 1 } }
+  x.report('10,000') { TEN_THOUSAND.times { 1 + 1 } }
+  x.report('100,000') { HUNDRED_THOUSAND.times { 1 + 1 } }
+  x.report('1,000,000') { MILLION.times { 1 + 1 } }
+  x.report('10,000,000') { TEN_MILLION.times { 1 + 1 } }
+  graph_points = [ 1, x.list[1].real / x.list[0].real,
+                  x.list[2].real / x.list[0].real, x.list[3].real / x.list[0].real,
+                  x.list[4].real / x.list[0].real, x.list[5].real / x.list[0].real,
+                  x.list[6].real / x.list[0].real ]
+end
 
-#
-# class Something
-#   def method_to_test_1
-#   end
-#
-#   def method_to_test_2
-#   end
-# end
+
+g = Gruff::Line.new
+g.title = 'Graph of Big O'
+g.labels = { 0 => "TEN", 1 => 'HUNDRED', 2 => 'THOUSAND', 3 => 'TEN THOUSAND',
+            4 => 'HUNDRED THOUSAND', 5 => 'MILLION', 6 => 'TEN MILLION' }
+
+g.data('first', graph_points)
+g.write('big_o_graph.png')
