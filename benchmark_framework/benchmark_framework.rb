a85be96421e8a771cccede25037a77b7612462e4
Jon Kimble <jonathanbkimble@gmail.com>
2017-09-08 16:50:29 -0700

    Remove graphing function and output rate of increase array instead

--- benchmark_framework/benchmark_framework.rb
+++ benchmark_framework/benchmark_framework.rb
@@ -1,7 +1,6 @@
 require 'benchmark'
-require 'code_to_benchmark'
+require './code_to_benchmark'
 require 'pry'
-require 'gruff'
 
 TEN = 10
 HUNDRED = 100
@@ -22,26 +21,22 @@ TEN_MILLION = 10_000_000
 
 # For testing O(n)
 
-graph_points = nil
-Benchmark.bm(7) do |x|
-  x.report('10') { TEN.times { 1 + 1 } }
-  x.report('100') { HUNDRED.times { 1 + 1 } }
-  x.report('1,000') { THOUSAND.times { 1 + 1 } }
-  x.report('10,000') { TEN_THOUSAND.times { 1 + 1 } }
-  x.report('100,000') { HUNDRED_THOUSAND.times { 1 + 1 } }
-  x.report('1,000,000') { MILLION.times { 1 + 1 } }
-  x.report('10,000,000') { TEN_MILLION.times { 1 + 1 } }
-  graph_points = [ 1, x.list[1].real / x.list[0].real,
-                  x.list[2].real / x.list[0].real, x.list[3].real / x.list[0].real,
-                  x.list[4].real / x.list[0].real, x.list[5].real / x.list[0].real,
-                  x.list[6].real / x.list[0].real ]
-end
-
-
-g = Gruff::Line.new
-g.title = 'Graph of Big O'
-g.labels = { 0 => "TEN", 1 => 'HUNDRED', 2 => 'THOUSAND', 3 => 'TEN THOUSAND',
-            4 => 'HUNDRED THOUSAND', 5 => 'MILLION', 6 => 'TEN MILLION' }
-
-g.data('first', graph_points)
-g.write('big_o_graph.png')
+# execution_time_increase_rates = Array.new
+# Benchmark.bm(7) do |x|
+#   x.report('10') { TEN.times { 1 + 1 } }
+#   x.report('100') { HUNDRED.times { 1 + 1 } }
+#   x.report('1,000') { THOUSAND.times { 1 + 1 } }
+#   x.report('10,000') { TEN_THOUSAND.times { 1 + 1 } }
+#   x.report('100,000') { HUNDRED_THOUSAND.times { 1 + 1 } }
+#   x.report('1,000,000') { MILLION.times { 1 + 1 } }
+#   x.report('10,000,000') { TEN_MILLION.times { 1 + 1 } }
+#   baseline = x.list[0].real
+#   previous_rate_of_increase = 1
+#   x.list.each do |real_time|
+#     execution_time_increase_rates << 'x' + ((real_time.real / baseline) / previous_rate_of_increase).to_s[0..3]
+#     previous_rate_of_increase = (real_time.real / baseline)
+#   end
+#   nil
+# end
+#
+# puts 'Rate of increase in execution time: '+ execution_time_increase_rates.to_s
