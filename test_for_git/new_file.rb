4faa96512a8df0c27d4456f40f129f4287846331
Jon Kimble <jonathanbkimble@gmail.com>
2017-10-16 07:35:20 -0700

    Add some extra stuff

--- test_for_git/new_file.rb
+++ test_for_git/new_file.rb
@@ -3,6 +3,10 @@ class DatClass
   def self.some_method(name)
     puts "Hello " + name
   end
+
+  def self.second_awesome_method(name)
+    puts "Hey #{name}, you're awesome!"
+  end
 end
 
 DatClass.some_method("Matilda")
--- test_for_git/second_file.rb
+++ test_for_git/second_file.rb
@@ -8,3 +8,9 @@ new_arr = arr.map do |num|
 end
 
 puts "This is the modified array: " + new_arr
+
+nums_in_english = {1 => "one", 2 => "two", 3 => "three",
+                   4 => "four", 5 => "five"}
+arr.each do |num|
+  puts nums_in_english[num]
+end
