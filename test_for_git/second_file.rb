c91a03ca2088c40b4b16fab25c99012433dc4050
Jon Kimble <jonathanbkimble@gmail.com>
2017-10-16 07:44:02 -0700

    Change order of methods

--- test_for_git/second_file.rb
+++ test_for_git/second_file.rb
@@ -2,6 +2,15 @@
 
 class RefactoredClass
 
+  def arr
+    [1,2,3,4,5]
+  end
+
+  def nums_in_english
+    {1 => "one", 2 => "two", 3 => "three",
+      4 => "four", 5 => "five"}
+    end
+
   def print_squares
     puts "This is the original array: " + arr
     new_arr = arr.map do |num|
@@ -10,14 +19,6 @@ class RefactoredClass
     puts "This is the modified array: " + new_arr
   end
 
-  def arr
-    [1,2,3,4,5]
-  end
-
-  def nums_in_english
-    {1 => "one", 2 => "two", 3 => "three",
-     4 => "four", 5 => "five"}
-  end
 
   def print_nums_in_english
     puts "Starting second method..."
